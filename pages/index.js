fd8c64119ab1d5f71d0540f787f2c21df3ef4b3d
Byleozovisk <136724865+Byleozovisk@users.noreply.github.com>
2023-09-13 13:02:50 -0300

    inclusão de arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from './index.module.scss';
 
 export default function Home() {
-    return <h1 className={styles.titulo}>Home</h1>
+    return <h1 className = { styles.titulo } > Home < /h1>
 }
