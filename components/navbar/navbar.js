df88f00bd48123464e987539cb651141ec0d8730
Byleozovisk <136724865+Byleozovisk@users.noreply.github.com>
2023-09-21 18:52:42 -0300

    Tentativa de conversão para react

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,11 +2,33 @@ import styles from './navbar.module.scss';
 
 export default function Navbar() {
     return (
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/contato">Contato</a></li>
-        </ul>
-      </nav>
-    )
-  }
+        <header className="header">
+          <nav className="nav">
+  
+          
+  
+            <a href="header_deslogado.html" className="home">
+              <img src="Icones/AlgarIcon.png" alt="Home" />
+            </a>
+  
+  
+            <section className="pesquisa">
+              <form className="barra-pesquisa">
+                <input type="text" className="barra-texto" placeholder="" />
+                <a href="#" className="botão-pesquisa">
+                  <img src="Icones/lupa.png" ealt="Pesquisar" />
+                </a>
+              </form>
+            </section>
+  
+            
+  
+            <ul className="menu">
+              <li><a href="header_deslogado.html">Cadastrar</a></li>
+              <li><a href="header_deslogado.html">Entrar</a></li>
+            </ul>
+  
+          </nav>
+        </header>
+      );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styles from './index.module.scss';
 
 export default function Home() {
-    return <h1 className = { styles.titulo } > Home < /h1>
+    return (oi)
+
 }
