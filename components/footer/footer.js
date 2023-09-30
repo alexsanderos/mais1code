9751aa905b8f55694853815622c5b5db8fe93104
André Freitas <andreodon@gmail.com>
2023-09-29 22:59:57 -0300

    Montagem do esquelo do footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,9 +1,31 @@
 import styles from './footer.module.scss';
 
 export default function Navbar() {
-    return (
-      <footer>
-        Footer
-      </footer>
-    )
+    return <> 
+        <div>
+          <section className={styles['sobre-nos']}>
+            <h4>Sobre nós</h4>
+            <a href='#' >Nossa História</a>
+          </section>
+          <section className={styles['redes-sociais']}>
+            <h4>Nossa Redes</h4>
+            <a href='#' ><img src='./imgs/Icones/facebook.png' /></a>
+            <a href='#' ><img src='./imgs/Icones/instagram.png' /></a>
+            <a href='#' ><img src='./imgs/Icones/youtube.png' /></a>
+            <a href='#' ><img src='./imgs/Icones/twitter.png' /></a>
+            <a href='#' ><img src='./imgs/Icones/linkedin.png' /></a>
+          </section>
+          <section className={styles['programas']}>
+            <h4>Programas</h4>
+            <a href='#' >Transforma</a>
+            <a href='#' >Voluntários</a>
+            <a href='#' >Talentos do Futuro</a>
+            <a href='#' >Educador do amanhã</a>
+          </section>
+          <section className={styles['ultima']}>
+            <img src='#'alt='logo algar'/>
+            <h5>Copyright © 2023 | Instituto Algar - Todos os direitos reservados.</h5>
+          </section>
+        </div>
+    </>
   }
